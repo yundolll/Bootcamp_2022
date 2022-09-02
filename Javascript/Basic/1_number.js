39931d195f98d772dff014574ef9caeb80637a3c
seoyunji <bellona07@naver.com>
2022-09-02 22:58:13 +0900

    js basic

---
+++ Javascript/Basic/1_number.js
@@ -0,0 +1,36 @@
+//Addition 덧셈
+50 + 5 //55
+
+//Subtraction 뺄셈
+90 - 1 //89
+
+//Multiplication  곱셈
+11111 * 7 // 77777
+
+//Division  나눗셈
+400 / 25 // 16
+
+//Modulo!!  나머지구하기
+27 % 2 // 1
+19 % 4 // 3
+
+
+
+//  NaN (Not a Number)  <- JS에서 숫자로 간주됨
+console.log(typeof NaN);  // number
+
+console.log(0/0); // NaN
+//수학과 산수에서는 0을 0으로 나누는건 무의미해서 NaN 판정
+
+//NaN은 JS에서 특별하다
+
+
+//Quiz
+
+console.log(4 + 3 * 4 / 2); //10
+
+console.log((13 % 4) ** 2 );  //1
+
+console.log((13 % 5) ** 2); //9
+
+console.log(200 + 0/0); //NaN
---
+++ Javascript/Basic/2_Variables.js
@@ -0,0 +1,42 @@
+// 변수 👜
+
+let year = 1999;
+//변수를 선언할 때는 세미콜론이 꼭 필요함
+
+let hens = 4;
+let roosters = 2;
+
+console.log(hens + roosters); // 6
+
+let totalChickens = hens + roosters;
+
+console.log(totalChickens); // 6
+
+hens++;
+
+console.log(hens);  //5
+console.log(totalChickens);
+
+
+//변수 업데이트하기 👩‍💻
+
+let score = 0;
+
+score = score + 5;
+console.log(score); // 5
+
+score += 5;
+console.log(score); // 10
+
+// score = score + 5 와 score += 5는 같다.
+
+let numLives = 9;
+console.log(numLives);
+console.log(numLives--);
+console.log(numLives);
+console.log(--numLives);
+console.log(numLives);
+
+console.log(++numLives);
+console.log(numLives++);
+console.log(numLives);
