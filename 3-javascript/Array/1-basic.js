df8819f559ff4c7e39e8077c0aae5cfda7246ecd
seoyunji <bellona07@naver.com>
2022-09-06 23:35:46 +0900

    Array basic

--- 3-javascript/Array/1-basic.js
+++ 3-javascript/Array/1-basic.js
@@ -6,7 +6,7 @@
 let students = [];
 
 //An array of strings
-let colors = ['red','orange','yellow'];
+// let colors = ['red','orange','yellow'];
 
 //An array of numbers
 let lottoNums = [19,22,45,12,34];
@@ -16,3 +16,33 @@ let stuff = [true,68,'dog',null];
 
 console.log(students.length); // 0
 console.log(lottoNums.length);  // 5
+
+
+
+// 배열 임의 접근
+
+// 배열은 각 요소에 인덱스가 부여되어 있음.
+console.log(stuff[2]);  // dog
+console.log(stuff[2].length);  // 3
+
+
+// Modifying Arrays 
+
+let colors = ['rad','orange','green','yalloww'];
+
+colors[0] = 'red';
+console.log(colors);
+
+colors[3] = 'yellow';
+console.log(colors);
+
+colors[10] = 'Indigo';
+console.log(colors);  // ['red', 'orange', 'green', 'yellow', empty × 6, 'Indigo']
+console.log(colors.length); // 11
+
+
+
+let beatles = ['paul','john','george'];
+
+beatles[3] = 'ringo';
+console.log(beatles); // (4) ['paul', 'john', 'george', 'ringo']
---
+++ 3-javascript/Array/2-Array_methods.js
@@ -0,0 +1,16 @@
+// ARRAY METHODS
+
+// push - add to end
+// pop - remove from end
+// shift - remove from start
+// unshift - add to start
+
+let movieLine = ['tom','nancy'];
+movieLine.push('jax');
+movieLine.push('pablo');
+movieLine.push('oliver');
+
+console.log(movieLine);
+
+movieLine.pop();  
+console.log(movieLine);
