964407e241f7676938b48d2c0f92451bbb5e8be3
seoyunji <bellona07@naver.com>
2022-09-06 23:48:54 +0900

    Update 2-Array_methods.js

--- 3-javascript/Array/2-Array_methods.js
+++ 3-javascript/Array/2-Array_methods.js
@@ -14,3 +14,30 @@ console.log(movieLine);
 
 movieLine.pop();  
 console.log(movieLine);
+
+
+// unshift
+let person = ['harry','potter','yun','ji'];
+person.unshift('you','hoo')
+console.log(person);
+// (6) ['you', 'hoo', 'harry', 'potter', 'yun', 'ji']
+
+
+
+// shift
+person.shift();
+console.log(person);
+// (5) ['hoo', 'harry', 'potter', 'yun', 'ji']
+
+
+// push와 pop은 끝에 작용하고, shift와 unshift는 시작에 작용
+
+
+// Push / Pop / Shift / Unshift Practice
+const planets = ['The Moon','Venus', 'Earth', 'Mars', 'Jupiter']; //DO NOT TOUCH THIS LINE!
+
+// YOUR CODE GOES BELOW THIS LINE:
+
+planets.shift();
+planets.push('Saturn');
+planets.unshift('Mercury');
