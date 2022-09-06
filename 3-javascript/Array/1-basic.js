7366b2a171f9f95c0c97f540224ab92487c2d23d
seoyunji <bellona07@naver.com>
2022-09-06 17:32:04 +0900

    Creating Arrays

---
+++ 3-javascript/Array/1-basic.js
@@ -0,0 +1,18 @@
+// Array(배열)
+// 1. 배열은 값의 집합이다. 
+// 2. 배열은 값에 순서가 있는 집합이다.
+
+// To make an empty array
+let students = [];
+
+//An array of strings
+let colors = ['red','orange','yellow'];
+
+//An array of numbers
+let lottoNums = [19,22,45,12,34];
+
+//A mixed array
+let stuff = [true,68,'dog',null];
+
+console.log(students.length); // 0
+console.log(lottoNums.length);  // 5
