add1b4ef4a8670ca9644b62d1e7faa4f841fe518
seoyunji <bellona07@naver.com>
2022-09-11 20:41:12 +0900

    Object

---
+++ 3-javascript/Object/1_create-object.js
@@ -0,0 +1,13 @@
+// JS Objects
+
+// 배열처럼 객체도 일종의 구조에 여러 데이터 조각을 저장하게 해줌
+// 프로퍼티라는 걸 이용해서 데이터가 저장됨 [키 : 값]
+
+
+// 객체 리터럴 만들기
+const person = {
+  firstName: 'Mick',
+  lastName: 'Jagger'
+}
+
+console.log(person);  // {firstName: 'Mick', lastName: 'Jagger'}
